038f9bd152dbcc4ced9c5fa569daf0ef66176ecc
gufronnakaaw <gufronnakaaw@gmail.com>
2026-08-03 00:36:55 +0700

    feat: add interceptor

---
+++ src/response.interceptor.ts
@@ -0,0 +1,23 @@
+import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { SKIP_INTERCEPTOR_KEY } from './skip.decorator';
+
+export class ResponseInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const skip = Reflect.getMetadata(
+      SKIP_INTERCEPTOR_KEY,
+      context.getHandler(),
+    );
+
+    if (skip) return next.handle();
+
+    return next.handle().pipe(
+      map((data) => {
+        const response: Record<string, unknown> = { success: true };
+        if (data) response.data = data;
+        return response;
+      }),
+    );
+  }
+}
