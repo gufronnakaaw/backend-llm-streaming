efcafe61f32b9306bf2134fbbf4fe339f4c03404
gufronnakaaw <gufronnakaaw@gmail.com>
2026-08-03 09:54:20 +0700

    feat: add cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { ResponseInterceptor } from './response.interceptor';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: { origin: ' * ' } });
   app.useGlobalInterceptors(new ResponseInterceptor());
 
   await app.listen(process.env.PORT ?? 3000);
