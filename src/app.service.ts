8389db13b541a778a7da77d89970139a2b1622fb
gufronnakaaw <gufronnakaaw@gmail.com>
2026-08-03 13:26:44 +0700

    chore: modified findLast function

--- src/app.service.ts
+++ src/app.service.ts
@@ -85,7 +85,9 @@ export class AppService {
     threadId: string,
     messages: { role: 'assistant' | 'user'; content: string }[],
   ) {
-    const lastUserMessage = messages.findLast((m) => m.role === 'user');
+    const lastUserMessage = messages.filter((m) => m.role === 'user')[
+      messages.length - 1
+    ];
 
     const res = await fetch(
       `${process.env.LLM_API_ENDPOINT}/v1/chat/completions`,
